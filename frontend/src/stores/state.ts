26c950921d083206d7159bc918dbc000e25a6757
Raj Saha <rajsaha@outlook.com>
2023-03-05 17:40:05 +0800

    Set date as UTC

--- frontend/src/stores/state.ts
+++ frontend/src/stores/state.ts
@@ -1,9 +1,11 @@
 import { defineStore } from "pinia"
 import dayjs from "dayjs"
+import utc from "dayjs/plugin/utc"
+dayjs.extend(utc)
 
 export const useCounterStore = defineStore("state", {
     state: () => ({
-        date: dayjs().format("YYYY-MM-DD"),
+        date: dayjs().utc().format("YYYY-MM-DD"),
     }),
     actions: {
         // since we rely on `this`, we cannot use an arrow function
